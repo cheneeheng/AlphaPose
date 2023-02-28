62c6056f0e460b0b35d020305014d6cb2878bffa
chen <eeheng.chen@tum.de>
2023-02-28 15:24:30 +0100

    Files modified after installing locally

--- alphapose/version.py
+++ alphapose/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Thu Jul 21 17:10:51 2022
+# TIME: Tue Feb 28 14:42:08 2023
 
-__version__ = '0.6.0+29ace8c'
-short_version = '0.6.0'
+__version__ = '0.5.0+'
+short_version = '0.5.0'
